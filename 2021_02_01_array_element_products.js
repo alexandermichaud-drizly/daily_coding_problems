4739f28e6e80cd7c7cfab706b4e6e3216e888d55
ARM <alexander.michaud@aya.yale.edu>
2021-02-01 13:22:28 -0500

    Measured performance

--- 2021_02_01_array_element_products.js
+++ 2021_02_01_array_element_products.js
@@ -1,3 +1,5 @@
+const performance = require('perf_hooks').performance;
+
 const getProducts = list => {
   const product = list.reduce((a, n) => a * n);
 
@@ -25,7 +27,28 @@ const getProductsWithoutDivision = list => {
   return outputList;
 }
 
-const test_list_1 = [1,2,3,4,5];
+const getProductsWithoutDivisionOptimized = list => {
+  let outputList = Array(list.length);
+  for (let i = 0; i < list.length; i++) {
+    outputList[i] = list.slice(0,i).concat(list.slice((i + 1), list.length)).reduce((a, r) => a * r);
+  }
+  return outputList;
+}
 
-console.log(getProducts(test_list_1));
-console.log(getProductsWithoutDivision(test_list_1));
+const test_list_1 = [1,2,3,4,5,6,7,8,9,10,11,12];
+
+const t1 = performance.now();
+getProducts(test_list_1);
+const t2 = performance.now();
+
+const t3 = performance.now();
+getProductsWithoutDivision(test_list_1);
+const t4 = performance.now();
+
+const t5 = performance.now();
+getProductsWithoutDivisionOptimized(test_list_1);
+const t6 = performance.now();
+
+console.log(`Efficiency of division: ${t2 - t1}`);
+console.log(`Efficiency of O(n^2) solution: ${t4 - t3}`);
+console.log(`Efficiency of efficient solution: ${t6 - t5}`);
