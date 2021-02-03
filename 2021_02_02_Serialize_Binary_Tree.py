629861615bc011e49da9f5abc00c8dac43cc9e88
ARM <alexander.michaud@aya.yale.edu>
2021-02-02 20:48:00 -0500

    Solved Feb 2

--- 2021_02_02_Serialize_Binary_Tree.py
+++ 2021_02_02_Serialize_Binary_Tree.py
@@ -8,23 +8,29 @@ def serialize(node):
     queue = [node]
     output = ''
     while queue:
-        if queue[0].left:
+        if queue[0] is not None:
             queue.append(queue[0].left)
-
-        if queue[0].right:
             queue.append(queue[0].right)
-
-        output = output + queue[0].val
+            output = output + '-' + queue[0].val
+        else:
+            output = output + '-' +'None'
         queue.pop(0)
 
     return output
             
+def nest(node_list, i=1):
+    if i >= len(node_list) or node_list[i] == 'None':
+        return None
+    else:
+        left = nest(node_list, (2*i))
+        right = nest(node_list, ((2*i) + 1))
+        return Node(node_list[i], left, right)
 
+def deserialize(serialized):
+    serialized_list = serialized.split('-')
 
-def deserialize(node):
-    return
+    return nest(serialized_list)
 
-
-test_node = Node('root', Node('left', Node('left.left')), Node('right'))
-print serialize(test_node)
-# assert deserialize(serialize(test_node)).left.left.val == 'left.left'
+test_node = Node('root', Node('left', Node('left.left', None, Node('left.left.right'))), Node('right', Node('right.left')))
+print(serialize(test_node))
+assert deserialize(serialize(test_node)).left.left.right.val == 'left.left.right'
