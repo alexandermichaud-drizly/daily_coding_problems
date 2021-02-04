fd54918d53f1dec13daa9724125af3d0b2446b05
ARM <alexander.michaud@aya.yale.edu>
2021-02-03 23:03:46 -0500

    Feb 3 solved

--- 2021_02_03_Missing_Lowest_Positive_Int.js
+++ 2021_02_03_Missing_Lowest_Positive_Int.js
@@ -25,26 +25,24 @@ const test_input_list_7 = [2,1,7,5];
 // };
 
 const findMissing = list => {
-  let max = 1;
   let omissions = 0;
+  let appendix = [];
   for (let i = 0; i < list.length; i++) {
-    if (list[i] > max) {
-      max = list[i];
-    }
     if (list[i] < 1) {
-      let appendix = list.splice(i, 1);
-      list.push(appendix[0])
+      appendix = appendix.concat(list.splice(i, 1));
       omissions += 1;
     }
   }
+  list = list.concat(appendix);
 
-  console.log(list);
   // Make values at index of extant values negative
-  for (let j = 1; j < (list.length - omissions + 1); j++) {
-      list[Math.abs(list[j]) - 1] = (-1)*(list[Math.abs(list[j]) - 1])
+  for (let j = 0; j < list.length - omissions; j++) {
+    let index = Math.abs(list[j]) - 1;
+    if (index < list.length - omissions) {
+      list[index] = (-1)*Math.abs(list[index]);
+    }
   }
 
-  console.log('List with negatives: ', list);
   // Find first positive
   let k = 0;
   while (k < list.length) {
@@ -53,7 +51,7 @@ const findMissing = list => {
     }
     k++;
   }
-  return max + 1;
+  return k;
 }
 
 const result1 = findMissing(test_input_list_1);
@@ -67,7 +65,7 @@ const result7 = findMissing(test_input_list_7);
 assert.deepEqual(result1, 2);
 assert.deepEqual(result2, 3);
 assert.deepEqual(result3, 3);
-//assert.deepEqual(result4, 4);
-//assert.deepEqual(result5, 1);
-//assert.deepEqual(result6, 3);
-//assert.deepEqual(result7, 3);
+assert.deepEqual(result4, 4);
+assert.deepEqual(result5, 5);
+assert.deepEqual(result6, 3);
+assert.deepEqual(result7, 3);
