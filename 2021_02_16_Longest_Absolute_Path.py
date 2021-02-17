06816d366986acc042cd41ef55ee3eddbdb17490
ARM <alexander.michaud@aya.yale.edu>
2021-02-16 19:05:07 -0500

    Note to self: probably don't need named tuples

--- 2021_02_16_Longest_Absolute_Path.py
+++ 2021_02_16_Longest_Absolute_Path.py
@@ -5,18 +5,29 @@ class Deserializer:
 
 	Longest = namedtuple('Longest', ['path', 'length'])
 
-	def __init__():
+	def __init__(self):
 		self.__file_system = None
 
 	def __new_file_system(self, root):
 		self.__file_system = Directory(root)
 
 	def __deserialize_helper(self, file_path):
-		longest = Longest('', 0)
+		longest = self.Longest('', 0)
 
-		if file_path[0] is not :
-			root = file_path.split()[0]
+		if not re.match(r'^\\n', file_path):
+			root = re.match(r'^[a-z0-9]+', file_path).group()
 			self.__new_file_system(root)
+			# Call helper on file_path minus root
+		else:
+			# Parse path for leading newline + number of tabs
+			# Split based on matches of identical number of \t
+			# For each subitem
+			#	if blank, return ('',0)
+			#	elif contains period, return (filename, length of file)
+			#	else call deserialize helper on everything from next \n to end. 
+			#		Create new directory objects
+			#		longest.length = abs_path length + longest.length if greater than longest.length 
+
 
 		return longest
 
@@ -41,8 +52,10 @@ class File:
 		self.abs_path = self.parent.name + "/" + name
 
 test_input_1 = r"dir\n\tsubdir1\n\tsubdir2\n\t\tfile1.ext"
-test_input_2 = r"dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\t\subsubdir2\n\t\t\tfile2.ext"
+test_input_2 = r"dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext"
 test_input_3 = r"dir\n\tsubdir1\n\t\tsubsubdir1" 
 
 driver = Deserializer()
-driver.deserialize()
+print(driver.deserialize(test_input_1))
+print(driver.deserialize(test_input_2))
+print(driver.deserialize(test_input_3))
