7db34232b0a0671e919afc042d48c576fb561b2b
ARM <alexander.michaud@aya.yale.edu>
2021-02-16 23:53:01 -0500

    Found a hilariously simpler solution to feb 16

--- 2021_02_16_Longest_Absolute_Path.py
+++ 2021_02_16_Longest_Absolute_Path.py
@@ -1,61 +1,30 @@
-from collections import namedtuple
 import re
 
-class Deserializer:
+def longest_file_path(path):
+	length = 0
+	split_path = re.split(r'\\n', path)
+	split_path.reverse()
 
-	Longest = namedtuple('Longest', ['path', 'length'])
+	for i, possible_file in enumerate(split_path):
+		if re.search(r'\.', possible_file):
+			depth = len(re.findall(r'\\t', possible_file)) - 1
+			j = i + 1
+			abs_path = re.split(r'\\t', possible_file)[-1]
+			while depth > -1:
+				possible_parent = split_path[j]
+				if len(re.findall(r'\\t', possible_parent)) == depth and not re.search(r'\.', possible_parent):
+					abs_path = re.split(r'\\t', possible_parent)[-1] + '/' + abs_path
+					depth -= 1
+				j += 1
+			print(abs_path)
+			length = max(length, len(abs_path))
 
-	def __init__(self):
-		self.__file_system = None
-
-	def __new_file_system(self, root):
-		self.__file_system = Directory(root)
-
-	def __deserialize_helper(self, file_path):
-		longest = self.Longest('', 0)
-
-		if not re.match(r'^\\n', file_path):
-			root = re.match(r'^[a-z0-9]+', file_path).group()
-			self.__new_file_system(root)
-			# Call helper on file_path minus root
-		else:
-			# Parse path for leading newline + number of tabs
-			# Split based on matches of identical number of \t
-			# For each subitem
-			#	if blank, return ('',0)
-			#	elif contains period, return (filename, length of file)
-			#	else call deserialize helper on everything from next \n to end. 
-			#		Create new directory objects
-			#		longest.length = abs_path length + longest.length if greater than longest.length 
-
-
-		return longest
-
-	def deserialize(self, file_path):
-		return self.__deserialize_helper(file_path).length
-		
-
-class Directory:
-	def __init__(self, name, parent=None):
-		self.name = name
-		self.parent = parent
-		self.abs_path = self.parent.name + "/" + self.name if parent is not None else self.name
-		self.contents = []
-
-	def add(self, item):
-			self.contents.append(item)
-
-class File:
-	def __init__(self, name, parent):
-		self.name = name
-		self.parent = parent
-		self.abs_path = self.parent.name + "/" + name
+	return length
 
 test_input_1 = r"dir\n\tsubdir1\n\tsubdir2\n\t\tfile1.ext"
 test_input_2 = r"dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext"
 test_input_3 = r"dir\n\tsubdir1\n\t\tsubsubdir1" 
 
-driver = Deserializer()
-print(driver.deserialize(test_input_1))
-print(driver.deserialize(test_input_2))
-print(driver.deserialize(test_input_3))
+print(longest_file_path(test_input_1))
+print(longest_file_path(test_input_2))
+print(longest_file_path(test_input_3))
