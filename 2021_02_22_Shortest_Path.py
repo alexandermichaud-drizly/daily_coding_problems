ab67a8e9b7bee05e5ad605e96875e573ac6be369
ARM <alexander.michaud@aya.yale.edu>
2021-02-23 09:28:31 -0500

    Finally!! I needed the memo to be passed as a copy to each position in the queue, not as a persistent log.

--- 2021_02_22_Shortest_Path.py
+++ 2021_02_22_Shortest_Path.py
@@ -4,18 +4,13 @@ class Matrix:
 
 	def __init__(self, matrix):
 		self.matrix = matrix
-		self.log = {}
 
-	def find_shortest_path(self,start,finish,last=None):
+	def find_shortest_path(self,start,finish, visited={}):
 		print(start)
-		print(self.log)
-		if last is None:
-			self.log[start] = 0
-			self.log[finish] = None
-		else:
-			steps = self.log[last] + 1
-			self.log[start] = steps if start not in self.log else min([steps, self.log[start]])
-			print(f'steps from last: {steps}; new log val: {self.log[start]}')
+		if start == finish:
+			return 0
+
+		visited[start] = True
 		
 		look_up, look_right, look_down, look_left =\
 			(start[0],     start[1] + 1), \
@@ -24,16 +19,20 @@ class Matrix:
 			(start[0] - 1, start[1]) 
 
 		look_around = [look_up, look_right, look_down, look_left]
+		min_path = None
 		for move in look_around:
-			if move == finish:
-				final_path = self.log[start] + 1
-				self.log[move] = final_path if self.log[finish] is None else min([final_path, self.log[move]])
-			elif move not in self.log and move[0] > -1 and move[1] > -1 and move[0] < self.matrix.shape[0] and move[1] < self.matrix.shape[1]:
+			if move not in visited and move[0] > -1 and move[1] > -1 and move[0] < self.matrix.shape[0] and move[1] < self.matrix.shape[1]:
 				if self.matrix[move[0],move[1]]:
-					self.log[move] = -1
+					visited[move] = True
 				else:
-					self.find_shortest_path(move,finish,start)	
-		return self.log[finish]
+					path = self.find_shortest_path(move,finish,visited.copy())
+					if path is None:
+						pass
+					elif min_path is None:
+						min_path = path + 1
+					else: 
+						min_path = min(min_path, path + 1)
+		return min_path
 			
 
 test_1 = np.array([\
@@ -42,5 +41,18 @@ test_1 = np.array([\
 	[0,0,0,0],\
 	[0,0,0,0]])
 
+test_2 = np.array([\
+	[0,0,0,0,0],\
+	[1,1,1,1,0],\
+	[0,0,0,1,0],\
+	[1,1,0,1,0],\
+	[0,0,0,0,0],\
+	[0,1,0,1,0],\
+	[0,1,0,0,0]])
+
+
 matrix = Matrix(test_1)
 print(matrix.find_shortest_path((0,0), (3,0)))
+matrix = Matrix(test_2)
+print(matrix.find_shortest_path((0,0), (6,0)))
+
