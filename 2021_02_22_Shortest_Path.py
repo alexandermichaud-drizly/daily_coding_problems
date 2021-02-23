23ad3dedad4d3927b4aa10fc22b771bfd5d808dc
ARM <alexander.michaud@aya.yale.edu>
2021-02-22 23:17:27 -0500

    Giving up on this one. Need to better understand how to do depth first search on non-tree

--- 2021_02_22_Shortest_Path.py
+++ 2021_02_22_Shortest_Path.py
@@ -8,12 +8,14 @@ class Matrix:
 
 	def find_shortest_path(self,start,finish,last=None):
 		print(start)
+		print(self.log)
 		if last is None:
 			self.log[start] = 0
 			self.log[finish] = None
 		else:
 			steps = self.log[last] + 1
-			self.log[start] = steps if start not in self.log or self.log[start] == None else min(steps, self.log[start])
+			self.log[start] = steps if start not in self.log else min([steps, self.log[start]])
+			print(f'steps from last: {steps}; new log val: {self.log[start]}')
 		
 		look_up, look_right, look_down, look_left =\
 			(start[0],     start[1] + 1), \
@@ -23,12 +25,14 @@ class Matrix:
 
 		look_around = [look_up, look_right, look_down, look_left]
 		for move in look_around:
-			if move not in self.log and move[0] > -1 and move[1] > -1 and move[0] < self.matrix.shape[0] and move[1] < self.matrix.shape[1]:
+			if move == finish:
+				final_path = self.log[start] + 1
+				self.log[move] = final_path if self.log[finish] is None else min([final_path, self.log[move]])
+			elif move not in self.log and move[0] > -1 and move[1] > -1 and move[0] < self.matrix.shape[0] and move[1] < self.matrix.shape[1]:
 				if self.matrix[move[0],move[1]]:
 					self.log[move] = -1
 				else:
-					self.find_shortest_path(move,finish,start)
-				print(self.log)
+					self.find_shortest_path(move,finish,start)	
 		return self.log[finish]
 			
 
